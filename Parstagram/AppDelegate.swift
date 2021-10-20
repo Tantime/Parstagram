b493cbe5e4c5c6399171312d5577c7ddd4771b64
Tantime <48768802+Tantime@users.noreply.github.com>
2021-10-20 00:33:52 -0700

    user can sign up and log in

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "NhYX1gHZw928LvCWHtL53bVhxspHkG6r6NMvb8Xx"
+                $0.clientKey = "Vfl4jmSXwvFIjsMPwGZ1JMHBonJY5oTLdYjyKoTH"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
